e8e6fd87b65a62280f5b7a9e411fd5643abe0306
mahesh <mahesh@devops.com>
2025-03-31 00:27:26 +0530

    updated pipeline

---
+++ sharedlibraries/vars/cicd.groovy
@@ -0,0 +1,16 @@
+def gitDownload(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+}
+def mavenBuild()
+{
+  sh "mvn package"
+}
+def tomcatDeploy(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+def executeSelinium(jobname)
+{
+ sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
